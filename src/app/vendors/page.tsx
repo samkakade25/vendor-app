e3029cddd4209be4470e3b1a193642d0966a12e5
sameer <rkvfc89@gmail.com>
2025-07-25 22:25:00 +0530

    Added logout feature

--- src/app/vendors/page.tsx
+++ src/app/vendors/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import { signOut } from "next-auth/react";
 
 async function getVendors() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/vendors`, {
@@ -107,8 +108,16 @@ export default function VendorPage() {
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 m-4">
+      <div className="flex justify-between">
       <h1 className="text-2xl font-bold mb-4">Vendor List</h1>
+      <button
+      onClick={() => signOut({ callbackUrl: "/" })}
+      className="bg-red-500 text-white px-4 py-2 mr-4 rounded hover:bg-red-600"
+    >
+      Logout
+    </button>
+    </div>
       <Link href="/vendors/new" className="text-blue-600 underline">
         Add Vendor
       </Link>
